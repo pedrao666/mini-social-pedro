89077bef3da1aba414126d8d5088d0135bbec8d4
pedrao666 <pedro.7574168@aluno.mg.gov.br>
2026-04-01 16:11:52 +0000

    alteracoe

--- js/script.js
+++ js/script.js
@@ -9,9 +9,24 @@ function curtir() {
   } else{
     likeCount--;
     curtido = false
-    document.getElementById("likeCount").innerText = likeCount;
+    document.getElementById("deslikeCount").innerText = likeCount;
   }
   
 }
-
+function descurtir() {
+  if(descurtido == false){
+    likeCount--;
+    descurtido = true;
+    document.getElementById("deslikeCount").innerText = likeCount;
+  } else{
+    likeCount--;
+    descurtido = false
+    document.getElementById("deslikeCount").innerText = likeCount;
+  }
+  
+}
 document.getElementById("likeBtn").addEventListener("click", curtir);
+
+document.getElementById("dselikeBtn").addEventListener("click", descurtir);
+
+let count = document.getElementById("likeCount");
